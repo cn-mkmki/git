21e65f74a1a7466a872b3d9940c55af48539612e
mkmki <1790582895@qq.com>
2021-06-28 18:47:35 +0800

    first 1

---
+++ src/main/java/aa.java
@@ -0,0 +1,11 @@
+/**
+ * @author mkmki
+ * @create 2021-06-28 下午 6:45:43
+ */
+public class aa {
+
+
+    public static void main(String[] args) {
+        System.out.println("\"aa\" = " + "aa");
+    }
+}
